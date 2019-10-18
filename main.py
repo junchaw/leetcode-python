14afe1f12df4713118a0e13950a500a69021f17a
wbswjc <me@wbswjc.com>
2019-10-18 23:54:10 +0800

    Dynamic import

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import argparse
+import importlib
 import time
-
-from problems import problem232, problem236, problem141, problem24, problem142, \
-    problem235, problem1, problem20, problem242, problem225, problem239, \
-    problem206, problem25, problem98, problem703
 
 
 def main():
@@ -13,36 +10,20 @@ def main():
 
     args = parser.parse_args()
 
-    problem = args.problem
+    problem_number = args.problem
 
-    if problem is None:
+    if problem_number is None:
         print("Which problem?")
         return
 
-    problems = {
-        1: problem1.solve,
-        20: problem20.solve,
-        24: problem24.solve,
-        25: problem25.solve,
-        98: problem98.solve,
-        141: problem141.solve,
-        142: problem142.solve,
-        206: problem206.solve,
-        225: problem225.solve,
-        232: problem232.solve,
-        235: problem235.solve,
-        236: problem236.solve,
-        239: problem239.solve,
-        242: problem242.solve,
-        703: problem703.solve,
-    }
-
-    if problem in problems:
+    try:
+        problem = importlib.import_module(
+            'problems.problem{}'.format(problem_number))
         start_time = time.time()
-        problems[problem]()
+        problem.solve()
         print("--- {} seconds used ---".format(time.time() - start_time))
-    else:
-        print("Problem {} not exist.".format(problem))
+    except ImportError as e:
+        print("Problem {} not exist.".format(problem_number))
 
 
 if __name__ == '__main__':
